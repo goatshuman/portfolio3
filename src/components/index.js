751fd7d5169d574e31209c2dd40b01a267660a1d
goatshuman <anshumanbahuguna94@gmail.com>
2026-05-12 18:29:19 +0530

    fix: remove Achievement from barrel — fixes RollupError build failure

--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,24 @@
 import About from "./About";
-import Achievement from "./Achievement";
-import { ComputersCanvas, EarthCanvas, StarsCanvas } from "./canvas";
-import Contact from "./Contact";
-import Feedbacks from "./Feedbacks";
-import Hero from "./Hero";
-import CanvasLoader from "./Loader";
-import Navbar from "./Navbar";
-import Preloader from "./preloader";
-import Works from "./Works";
+  import { ComputersCanvas, EarthCanvas, StarsCanvas } from "./canvas";
+  import Contact from "./Contact";
+  import Feedbacks from "./Feedbacks";
+  import Hero from "./Hero";
+  import CanvasLoader from "./Loader";
+  import Navbar from "./Navbar";
+  import Preloader from "./preloader";
+  import Works from "./Works";
 
-export {
-  About,
-  Achievement,
-  CanvasLoader,
-  ComputersCanvas,
-  Contact,
-  EarthCanvas,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Preloader,
-  StarsCanvas,
-  Works,
-};
+  export {
+    About,
+    CanvasLoader,
+    ComputersCanvas,
+    Contact,
+    EarthCanvas,
+    Feedbacks,
+    Hero,
+    Navbar,
+    Preloader,
+    StarsCanvas,
+    Works,
+  };
+  
