3aca2700b8003e176cd90913e08864aac1d56fe5
goatshuman <anshumanbahuguna94@gmail.com>
2026-05-12 12:20:08 +0530

    fix: Wrap components in ErrorBoundary to prevent black screen after preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,73 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {
-  About,
-  Contact,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Preloader,
-  StarsCanvas,
-  Works,
-} from "./components";
-import EasterEggs from "./components/EasterEggs";
-import ElasticCursor from "./components/ElasticCursor";
-import SkillKeyboard from "./components/SkillKeyboard";
-import ReactBitsAudioProvider from "./reactbits/context/ReactBitsAudioProvider";
-import ReactBitsCursorProvider from "./reactbits/context/ReactBitsCursorProvider";
+  import {
+    About,
+    Contact,
+    Feedbacks,
+    Hero,
+    Navbar,
+    Preloader,
+    StarsCanvas,
+    Works,
+  } from "./components";
+  import EasterEggs from "./components/EasterEggs";
+  import ElasticCursor from "./components/ElasticCursor";
+  import SkillKeyboard from "./components/SkillKeyboard";
+  import ReactBitsAudioProvider from "./reactbits/context/ReactBitsAudioProvider";
+  import ReactBitsCursorProvider from "./reactbits/context/ReactBitsCursorProvider";
+  import ErrorBoundary from "./components/ErrorBoundary";
 
-const App = () => {
-  return (
-    <ReactBitsCursorProvider>
-      <ReactBitsAudioProvider>
-        <Preloader>
-          <BrowserRouter>
-            <div
-              className="relative z-0"
-              style={{ backgroundColor: "hsl(222.2 84% 4.9%)" }}
-            >
-              <ElasticCursor />
-              <EasterEggs />
-              <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-                <Navbar />
-                <Hero />
-              </div>
-              <About />
-              <Works />
-              <SkillKeyboard />
-              <Feedbacks />
-              <div className="relative z-0">
-                <Contact />
-                <StarsCanvas />
-              </div>
-            </div>
-          </BrowserRouter>
-        </Preloader>
-      </ReactBitsAudioProvider>
-    </ReactBitsCursorProvider>
-  );
-};
+  const App = () => {
+    return (
+      <ErrorBoundary>
+        <ReactBitsCursorProvider>
+          <ReactBitsAudioProvider>
+            <Preloader>
+              <BrowserRouter>
+                <div
+                  className="relative z-0"
+                  style={{ backgroundColor: "hsl(222.2 84% 4.9%)" }}
+                >
+                  <ErrorBoundary>
+                    <ElasticCursor />
+                  </ErrorBoundary>
+                  <ErrorBoundary>
+                    <EasterEggs />
+                  </ErrorBoundary>
+                  <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar />
+                    <ErrorBoundary>
+                      <Hero />
+                    </ErrorBoundary>
+                  </div>
+                  <ErrorBoundary>
+                    <About />
+                  </ErrorBoundary>
+                  <ErrorBoundary>
+                    <Works />
+                  </ErrorBoundary>
+                  <ErrorBoundary>
+                    <SkillKeyboard />
+                  </ErrorBoundary>
+                  <ErrorBoundary>
+                    <Feedbacks />
+                  </ErrorBoundary>
+                  <div className="relative z-0">
+                    <ErrorBoundary>
+                      <Contact />
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                      <StarsCanvas />
+                    </ErrorBoundary>
+                  </div>
+                </div>
+              </BrowserRouter>
+            </Preloader>
+          </ReactBitsAudioProvider>
+        </ReactBitsCursorProvider>
+      </ErrorBoundary>
+    );
+  };
 
-export default App;
+  export default App;
+  
